7564f3278d68d4d880aef16c30509c41bc6530de
AljosaNj <aljosaa.njezic@gmail.com>
2022-08-08 00:43:54 +0200

    hover update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -54,9 +54,9 @@ const handleScroll = () => {
 </ul>
 <div className="hamburger" onClick={handleClick}>
   {click ? (
-    <FaTimes size={20} style={{color: "#fff"}}/>
+    <FaTimes className="iconhamb"  size={20} /*style={{color: "#fff"}}*//>
   ): (
-    <FaBars size={20} style={{color: "#fff"}}/>
+    <FaBars  className="iconhamb"  size={20}/* style={{color: "#fff"}}*//>
   )}
 
 
