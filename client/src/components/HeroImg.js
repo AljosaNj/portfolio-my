624b402d71e70440c2ca7b1bed1cbe5f7c03f41a
AljosaNj <aljosaa.njezic@gmail.com>
2022-11-02 21:34:54 +0100

    hero animations

--- client/src/components/HeroImg.js
+++ client/src/components/HeroImg.js
@@ -2,7 +2,7 @@ import "./HeroImgStyles.css";
 import React from "react";
 import IntroImg from "../assets/heroprogram.jpg"
 import { Link } from "react-router-dom";
-
+import Typed from 'react-typed';
 
 const HeroImg = () => {
   return (
@@ -10,8 +10,25 @@ const HeroImg = () => {
     <div className="mask">
    <img className="into-img" src={IntroImg} alt="IntroImg"/>
     </div>
+
     <div className="content">
-      <p>HI,I`M A FREELANCER</p>
+  <Typed
+  className="animations-p"
+       strings={[
+        "HI,THIS IS MY PORTFOLIO",
+        "YOU CAN SEE SOME OF MY PROJECTS",
+        "CONTACT ME IF YOU ARE INTERESTED",
+       ]}
+       
+        typeSpeed={150}
+        backSpeed={100}
+      
+        loop
+      
+      />
+
+
+      {/* <p>HI,I`M A FREELANCER</p>*/}
       <h1>React Developer</h1>
       <div>
         <Link to="/project" className="btn">Projects</Link>
