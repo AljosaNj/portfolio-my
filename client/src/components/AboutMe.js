4a8fd597e285661042018b34500302600431fbd5
AljosaNj <aljosaa.njezic@gmail.com>
2023-01-24 20:21:42 +0100

    portfolio update

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -13,20 +13,12 @@ const AboutMe = () => {
                             <h1 className="about__heading skilltitle ">Who am I?</h1>
                             <div className="about__meta">
                             <p className="about__text p__color">
-                    Hello, my name is Aljosa Njezic and I'm a Junior  Developer. 
-  I'm basing myself on Web Development as a Frontend Developer. 
-  I am currently working with the ReactJS(JavaScript library).
-  I also know the basics of working in NodeJS and MongoDB.
+                   I'm a junior React developer and I've been learning Front-end web development for the last two years on my own. Some of the resources that I use for learning are platforms like Udemy, Pluralsight, YouTube, w3schools.​com, freeCodeCamp.​org, codepen.​io, 30secondsofcode.​org etc.
               </p>
               <p className="about__text p__color">
-            My goal is to become a Full Stack Developer(MERN).  
-I learned about programming online. On websites such as Udemy, Pluralsight, Youtube and other sites about programming.
-I also had experienced programmers as mentors.
+           Every day I'm working on real projects where I'm applying everything learned so far. I like challenges and I like to learn new things that I can use to solve problems.
               </p>
-              <p className="about__text p__color">
-            I like challenges and am open to the upcoming stages in the job...
-                    You can view my CV.
-              </p>
+            
               <div className="about__button d__flex align__items__center">
                     <a  href='aljosa-njezic.pdf' download='aljosa-njezic.pdf'   >
                  <button  className="btn"  >Download CV</button> 
